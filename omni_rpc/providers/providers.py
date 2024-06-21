cb50a48c0ed1816f1d91cc094f6df5fa10f8dc07
jacekv <jaca347@gmail.com>
2024-06-21 20:22:42 +0200

    Adding some stuff

--- omni_rpc/providers/providers.py
+++ omni_rpc/providers/providers.py
@@ -1,4 +1,8 @@
+import json
 import os
+import re
+
+import requests
 
 PATH_TO_CHAINS = f"{os.getcwd()}/omni_rpc/chains/_data/chains"
 SUPPORTED_CAIP_2_NAMESPACES = ["eip155"]
@@ -19,3 +23,79 @@ def check_provider_availability(chain_id: int) -> bool:
         if os.path.exists(f"{PATH_TO_CHAINS}/{filename}"):
             return True
     return False
+
+
+def get_chain_providers(chain_id: int) -> list:
+    """
+    Get the providers for the given chain_id.
+
+    Args:
+        chain_id (int): The chain_id for which the providers are to be fetched.
+
+    Returns:
+        dict: The providers for the given chain_id.
+    """
+    namespace = SUPPORTED_CAIP_2_NAMESPACES[0]
+    filename = f"{namespace}-{chain_id}.json"
+    with open(f"{PATH_TO_CHAINS}/{filename}", "r") as f:
+        chain_data = json.load(f)
+    providers = __filter_providers(chain_data["rpc"])
+    return providers
+
+
+def __filter_providers(providers: list) -> list:
+    """
+    Filters the providers to remove the ones that require an api key.
+
+    Args:
+        providers (list): The list of providers.
+
+    Returns:
+        list: The filtered list of providers.
+    """
+    # We filter rpc providers which have a placeholder ${...} and start
+    # with wss://
+    return [
+        provider
+        for provider in providers
+        if not re.search(r".*\$\{.*\}|wss:\/\/.*", provider)
+    ]
+
+
+def forward_request(message: dict, providers: list) -> dict:
+    """
+    Forwards the request to the providers and merges the response.
+
+    Args:
+        message (dict): The message to be forwarded.
+        providers (list): The list of providers.
+    
+    Returns:
+        dict: The merged response from the providers.
+    
+    Raises:
+        HTTPError: If the status code is not 200.
+    """
+    number_of_providers = len(providers)
+    responses = {}
+    for provider in providers:
+        print(f"Forwarding request to {provider}")
+        response = requests.post(provider, json=message)
+        if number_of_providers == 1 and response.status_code != 200:
+            response.raise_for_status()
+        if response.status_code != 200:
+            continue
+
+        # we check if there is an error in the response
+        data = response.json()
+        if "error" in data:
+            continue
+
+        data = json.dumps(data)
+        if data not in responses:
+            responses[data] = 1
+        else:
+            responses[data] += 1
+
+    selected_response = max(responses, key=responses.__getitem__)
+    return json.loads(selected_response)
--- omni_rpc/routers/proxy_rpc.py
+++ omni_rpc/routers/proxy_rpc.py
@@ -1,22 +1,25 @@
 from fastapi import APIRouter
 from fastapi import Request
+from fastapi import Response
+from starlette.status import HTTP_404_NOT_FOUND
 
 from omni_rpc.providers.providers import check_provider_availability
+from omni_rpc.providers.providers import forward_request
+from omni_rpc.providers.providers import get_chain_providers
 
 router = APIRouter()
 
 
-@router.post("/", tags=["users"])
-async def read_users(chain_id: int, request: Request):
+@router.post("/")
+async def read_users(chain_id: int, request: Request, response: Response):
     print("ChainID", chain_id)
-    print(check_provider_availability(chain_id))
-    # Steps:
-    # Check if the chain_id is valid by checking the files
-    # If valid, then get the providers
-    # If not valid, return error
-    # Forward request to providers
-    # MErge response and return
+    if not check_provider_availability(chain_id):
+        response.status_code = HTTP_404_NOT_FOUND
+        return {"detail": "Chain id not found."}
+
+    providers = get_chain_providers(chain_id)
+
     body = await request.json()
-    print(body)
-    print(__file__)
-    return {"message": "Hey, It is me Goku"}
+    response_body = forward_request(body, providers)
+
+    return response_body
