0d507ce003144ee2e2b74b9e32262932f55bc5e6
jacekv <jaca347@gmail.com>
2024-08-30 19:16:46 +0200

    Adding unique trace id

--- omni_rpc/main.py
+++ omni_rpc/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
+from fastapi import Request
 
-from omni_rpc.custom_logging import logger
+import omni_rpc.custom_logging as custom_logging
 from omni_rpc.routers import proxy_rpc
 
 app = FastAPI()
 
 
-logger.info("Starting Omni RPC server")
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    custom_logging.trace_id = custom_logging.get_trace_id()
+    response = await call_next(request)
+    return response
+
+
 app.include_router(proxy_rpc.router)
